8347e0ae564d8569f5e64b713bdf7f1486a3703e
JPM1919 <153214837+JPM1919@users.noreply.github.com>
2023-12-10 18:18:15 +0100

    texto en ventana juego

--- src/juegozombies/VentanaJuego.java
+++ src/juegozombies/VentanaJuego.java
@@ -5,9 +5,12 @@
 package juegozombies;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -17,6 +20,7 @@ import javax.swing.JPanel;
 public class VentanaJuego extends javax.swing.JFrame {
 
     JFrame frame = new JFrame();
+    JPanel seguimiento = new JPanel();
     JPanel tablero = new JPanel(new GridLayout(Juego.getTamanoCuadricula().getX(), Juego.getTamanoCuadricula().getY()));
     JPanel acciones = new JPanel(new GridLayout(1, 3));
     JButton celda[][] = new JButton[Juego.getTamanoCuadricula().getX()] [Juego.getTamanoCuadricula().getY() ];
@@ -25,13 +29,13 @@ public class VentanaJuego extends javax.swing.JFrame {
      */
     public VentanaJuego() {
         frame.setTitle("HOLA");
-        frame.setSize(800, 800);
+        frame.setSize(1000, 800);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       
         for (int i=0; i<Juego.getTamanoCuadricula().getX()  ; i++){
-            for(int j = 0; j< Juego.getTamanoCuadricula().getX(); j++){
-                celda[i][j] = new JButton(String.valueOf(""));
+            for(int j = 0; j< Juego.getTamanoCuadricula().getY(); j++){
+                celda[i][j] = new JButton((String.valueOf(i*Juego.getTamanoCuadricula().getY()+j)));
                 if(Juego.getSupervivientes().get(0).devolverCoordenada().getX() == i && Juego.getSupervivientes().get(0).devolverCoordenada().getY() == i ){
                     for(int l = 0; l< Juego.getSupervivientes().size(); l++){
                         celda[i][j].setText(Juego.getSupervivientes().get(l).getNombre() + " , ");
@@ -47,9 +51,13 @@ public class VentanaJuego extends javax.swing.JFrame {
         JButton buscar = new JButton("BUSCAR");
         acciones.add(atacar);
         acciones.add(mover);
-        acciones.add(buscar);       
+        acciones.add(buscar);
+        JLabel texto = new JLabel("texto aqui");
+        seguimiento.add(texto);
         frame.add(tablero, BorderLayout.CENTER);
         frame.add(acciones, BorderLayout.SOUTH);
+        frame.add(seguimiento, BorderLayout.EAST);
+        seguimiento.setPreferredSize(new Dimension(300, 0));
         atacar.addActionListener(new java.awt.event.ActionListener() {
             @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
